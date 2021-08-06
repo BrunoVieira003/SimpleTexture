7b37b2b4a30543b92d4710cc8cd57b69976b0060
unknown <brunosilverio01@gmail.com>
2021-08-06 18:32:16 -0300

    Added user settings file
    User can now set a fixed theme for the app.
    This file is managed by the GenericDatabase class which is used inside the
    MainWindow class to send some data to the visual part of the application.

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 import sys
-from typing import Optional
+import os
+import json
 
 from PySide6.QtCore import QPropertyAnimation
-from gui.windows.mainwindow import UI_MainWindow
-
 from PySide6.QtWidgets import QApplication, QMainWindow
+
+from gui.windows.mainwindow import UI_MainWindow
+from model.general import GenericDatabase
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
 
         self.ui = UI_MainWindow()
-        self.ui.setup_ui(self, theme="default")
+        self.ui.setup_ui(self)
+        self.settings_data = GenericDatabase("data\\settings.json")
 
-        self.show()
+        # Themes
+        for theme in os.listdir("gui\\themes"):
+            theme = theme[:-5]
+            self.ui.add_theme(theme)
+        theme_index = self.ui.ui_settings.themes.findText(self.settings_data.get("theme").title())
+        self.ui.ui_settings.themes.setCurrentIndex(theme_index)
+        
+        self.set_window_theme(self.settings_data.get("theme"))
+
+        # Binding
+        self.ui.ui_settings.themes.currentIndexChanged.connect(self.on_theme_changed)
+
+    def on_theme_changed(self):
+        self.settings_data.set("theme", self.ui.ui_settings.themes.currentText().lower())
+        self.set_window_theme(self.settings_data.get("theme"))
+        
     
-    
+    def set_window_theme(self, theme):
+        with open(f"gui\\themes\\{theme}.json", "r") as arc:
+            theme_data = json.load(arc)
+            self.ui.set_theme(theme_data, theme)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    window.show()
     sys.exit(app.exec())
